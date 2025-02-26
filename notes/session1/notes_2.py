ecdb56e32d76126f424bd0aa0dcb487974001acd
Kevin Beideman <112713469+SkiritaiKevin@users.noreply.github.com>
2025-02-26 16:51:37 -0500

    Add files via upload

---
+++ notes/session1/notes_2.py
@@ -0,0 +1,28 @@
+#name = "ava ok"
+#print(name.title())
+
+"""
+#name = "ava ok"
+print(name.title())
+
+name.upper()
+name.lower()
+"""
+
+"""
+first = "kev"
+last = "awesome"
+full = f"{first} {last}"
+print(full)
+
+these are called f strings
+"""
+
+#print("\tsup") this creates a tab of whitespace, \n for newline
+"""name = "printsss "
+print(name.rstrip())""" #rstrip is used to remove any whitespace on right, lstrip for left, strip for generality
+
+"""url="https://www.roblox.com"
+print(url.removeprefix("https://"))
+"""
+
