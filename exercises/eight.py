bf5f0c092cae3885f87368d187f51de5e4c6ce19
Kevin Beideman <112713469+SkiritaiKevin@users.noreply.github.com>
2025-02-26 16:52:18 -0500

    Add files via upload

---
+++ exercises/eight.py
@@ -0,0 +1,7 @@
+# print(4+4)
+# print(10-2)
+# print(4*2)
+# print(64/8)
+
+fav=20
+print(f"My fav num is {fav}")
---
+++ exercises/personal_message.py
@@ -0,0 +1,31 @@
+#name="eric"
+#print(f"Hello {name}, would you like to learn some Python today?")
+
+"""
+#name_cases
+print(name.upper())
+print(name.lower())
+print(name.title())
+"""
+
+"""
+famous_quote
+
+quote='“A person who never made a mistake never tried anything new.”'
+print(f"Albert Einstein once said, {quote}")
+"""
+
+"""
+stripping names
+name="\tjohnsondavidsanchezmaria\t\n"
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+"""
+
+"""
+file extensions
+"""
+file="python_notes.txt"
+print(file.removesuffix(".txt"))
